a45f97a86aa2de1fb88798fa2e514bbb342b43f3
Eric Marty <allopole@noreply.github.com>
2020-03-30 08:02:14 -0400

    Daily update for March 30, 2020

--- R/data.R
+++ R/data.R
@@ -98,3 +98,17 @@ fatalities.US <- fatalities.US.wikipedia %>%
 
 remove(fatalities.US.wikipedia)
 saveRDS(fatalities.US,"data/fatalities.US.rds")
+
+# # Get country level data
+# US.counties <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+# 
+# US.counties <- US.counties %>% 
+#   mutate(fips = ifelse(is.na(fips),paste0(county,", ",state),fips))
+# 
+# cases.US.counties <- US.counties %>% select(-deaths)
+# fatalities.US.counties <- US.counties %>% select(-cases)
+# 
+# fips.10deaths <- fatalities.US.counties[fatalities.US.counties$deaths >= 10,]$fips %>% unique
+# 
+# fatalities.US.counties.10deaths <- fatalities.US.counties %>% 
+#   filter(fips %in% fips.10deaths)
